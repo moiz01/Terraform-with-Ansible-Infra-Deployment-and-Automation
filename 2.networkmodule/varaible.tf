0b606e150e586a43f14789b86a3392eeeb088cd5
moiz01 <moiz.cooltech@gmail.com>
2023-03-15 13:14:53 +0530

    Add files via upload

---
+++ 2.networkmodule/varaible.tf
@@ -0,0 +1,29 @@
+variable "AWS_ACCESS_KEY" {
+    type = string
+    default = "AKIAS6HD3C5BMSUA4L52"
+}
+
+variable "AWS_SECRET_KEY" {}
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "public_key_path" {
+  description = "Public key path"
+  default = "~/.ssh/levelup_key.pub"
+}
+
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default = "ami-05692172625678b4e"
+}
+
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default = "t2.micro"
+}
+
+variable "environment_tag" {
+  description = "Environment tag"
+  default = "Production"
+}
